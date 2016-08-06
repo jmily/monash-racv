cd150f1962ecc862914d35d577a5624a4ed85e66
jmily <weiyi.li713@gmail.com>
2016-08-07 09:27:17 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+monash-racv
+===========
+
+A Symfony project created on August 6, 2016, 11:16 am.
---
+++ src/AppBundle/Command/RacvADCommand.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\ProgressBar;
+
+class RacvADCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('racv:ad-dd')
+            ->setDescription('Appending AD DD');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $conn = $this->getContainer()->get('doctrine')->getConnection();
+
+        $query = "SELECT * FROM `c_e` GROUP BY `PAR_ID`,`VEH_REG_ID`";
+
+        $dataToAppend = $conn->fetchAll($query);
+
+        $numberOfQuery = (count($dataToAppend));
+
+        $output->setVerbosity($output::VERBOSITY_DEBUG);
+        $progress = new ProgressBar($output, $numberOfQuery);
+
+        $output->writeln([
+            'Inserting data',
+            '============',
+            '',
+        ]);
+        foreach ($dataToAppend as $d) {
+            $parId = $d['PAR_ID'];
+            $vehId = $d['VEH_REG_ID'];
+            $q = "SELECT * FROM `c_e` WHERE `PAR_ID` = '$parId' AND `VEH_REG_ID`='$vehId' ORDER BY `PAR_ID` DESC, `VEH_REG_ID` DESC, `ST_DTE` DESC";
+            $data = $conn->fetchAll($q);
+
+            $numberOfResult = count($data);
+            if ($numberOfResult == 1) {
+                $newestCancellationDate = '2100-01-01';
+                if ($data[0]['CANCD_DTE'] != '0000-00-00 00:00:00') {
+                    $newestCancellationDate = $data[0]['CANCD_DTE'];
+                    $newestStartDate = $data[0]['ST_DTE'];
+                    $oldestStartDate = $newestStartDate;
+                }
+
+            } else if ($numberOfResult > 1) {
+
+                $newestStartDate = $data[0]['ST_DTE'];
+                $oldestStartDate = $data[$numberOfResult - 1]['ST_DTE'];
+
+                $q2 = "SELECT `CANCD_DTE` FROM `c_e` WHERE `PAR_ID` = '$parId' AND `VEH_REG_ID` = '$vehId' ORDER BY `CANCD_DTE` DESC ";
+                $result = $conn->fetchAll($q2);
+
+                $newestCancellationDate = $result[0]['CANCD_DTE'];
+                if ($newestCancellationDate == '0000-00-00 00:00:00') {
+                    $newestCancellationDate = '2100-01-01';
+                }
+            }
+
+            $ad = $oldestStartDate;
+            if (strtotime($newestCancellationDate) > strtotime($newestStartDate)) {
+                $dd = $newestCancellationDate;
+            } else {
+                $dd = '2100-01-01';
+            }
+
+
+            $insertSql = "INSERT INTO `unicon_ad_dd` (`PAR_ID`,`VEH_REG_ID`,`AD`,`DD`) VALUES ( '$parId', '$vehId', '$ad', '$dd') ";
+            $conn->query($insertSql);
+
+            $progress->advance();
+        }
+        $progress->finish();
+
+    }
+}
